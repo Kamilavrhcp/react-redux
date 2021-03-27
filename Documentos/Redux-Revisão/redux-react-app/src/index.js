990c8e67d1822ca32541c038ca25914934ea700d
Marina Alane <marina.alane@outlook.com>
2021-03-27 16:34:03 -0300

    store

--- Documentos/Redux-Revisão/redux-react-app/src/index.js
+++ Documentos/Redux-Revisão/redux-react-app/src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// A união entre Redux e React
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={ store }>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// root responsável por renderizar tudo do html
+// provider vai receber todo o conteúdo da store

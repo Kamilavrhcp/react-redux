2f58b4af5efd819729633fd2ade93657c4c9e057
Marina Alane <marina.alane@outlook.com>
2021-03-27 17:00:21 -0300

    finalizando reducer

--- Documentos/Redux-Revisão/redux-react-app/src/App.js
+++ Documentos/Redux-Revisão/redux-react-app/src/App.js
@@ -1,21 +1,60 @@
-import { Component } from 'react'
+import { Component } from 'react';
+import { connect } from 'react-redux';
+import inputAction from './redux/actions/inputActions'
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      shouldAppear: false
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.clickChange = this.clickChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { inputChange } = this.props;
+    const { name, value } = target;
+    if(name === 'text') {
+      inputChange(value);
+      this.setState({
+        shouldAppear: false,
+      })
+    }
+  }
+
+  clickChange() {
+    this.setState({
+      shouldAppear: true,
+    })
+  }
 
   render() {
+    const { shouldAppear } = this.state;
+    const { inputState } = this.props;
     return (
       <section className="App-section">
         <header>
           <h1>React Redux</h1>
         </header>
         <label htmlFor="text-input">
-          <input name="text" id="text-input" type="text" />
+          <input onChange={this.handleChange} name="text" id="text-input" type="text" />
         </label>
-        <button type="button">Salvar</button>
+        <button onClick={() => this.clickChange} type="button">Salvar</button>
+        {shouldAppear && <p>{inputState}</p>}
       </section>
     );
   }
 }
 
-export default App;
+// Pegaremos as funções das actions
+const mapDispatchToProps = (dispatch) => ({
+  inputChange: (text) => dispatch(inputAction(text)),
+});
+
+// Pegamos o estado nos reducers
+const mapStateToProps = (state) => ({
+  inputState: state.inputReducer.text,
+});
+export default connect(mapStateToProps, mapDispatchToProps)(App);
